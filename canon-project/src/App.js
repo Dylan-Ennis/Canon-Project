99b813ae859fabfce9fce626fc9c64e13ff7f1a0
Dylan Ennis <dennis@ucsd.edu>
2024-05-09 21:03:56 -0700

    testing deployment

--- canon-project/src/App.js
+++ canon-project/src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div style={{ backgroundColor: 'black', padding: '10px' }}>
+      <h1 style={{color:'#b8860b',margin: '0'}}>Canon</h1>
+      </div>
       <header className="App-header">
-<h1>Tup tup tup</h1>
       </header>
+      
     </div>
   );
 }
